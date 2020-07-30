136b2d20bb5b17e132bd8cf57bd0355e32068cfb
mkh3030 <mkh3030@naver.com>
2020-07-30 21:40:48 +0900

    commit

--- src/main/java/com/example/cacheh2/Cacheh2Application.java
+++ src/main/java/com/example/cacheh2/Cacheh2Application.java
@@ -1,10 +1,22 @@
 package com.example.cacheh2;
 
+import java.nio.charset.Charset;
+
+import javax.servlet.Filter;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableScheduling
 @MapperScan("com.example.cacheh2.mapper")
@@ -12,5 +24,18 @@ public class Cacheh2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Cacheh2Application.class, args);		
 	}
+
+	@Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
 
 }
